6bd97ef337911437c2fc5c29132a9744d005c941
Kevin Luca Bochenski Badellino <67221885+OgataiKhan@users.noreply.github.com>
2023-12-07 14:20:47 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import JumbotronComponent from './components/AppHero.vue';
+
+export default {
+  name: 'AppComponent',
+  components: {
+    JumbotronComponent,
+  },
+};
+</script>
+
+<template>
+  <JumbotronComponent />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,16 @@
+<script>
+
+
+export default {
+  name: 'JumbotronComponent',
+};
+
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+  
+</style>
